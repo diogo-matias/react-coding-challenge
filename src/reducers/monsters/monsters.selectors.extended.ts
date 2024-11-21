0dc0edce7f0cacde743ab6a6dbe7a86d122322bf
Diogo Matias <diogomaccedo200@gmail.com>
2024-11-21 12:58:14 -0300

    .

--- src/reducers/monsters/monsters.selectors.extended.ts
+++
@@ -1,6 +0,0 @@
-import { RootState } from '../../app/store';
-
-export const selectMonsterWins = (state: RootState) =>
-  state.monstersExtended.winner?.winner;
-export const selectRandomMonster = (state: RootState) =>
-  state.monstersExtended.selectRandomMonster;
--- src/reducers/monsters/monsters.selectors.ts
+++
@@ -1,6 +0,0 @@
-import { RootState } from '../../app/store';
-
-export const selectMonsters = (state: RootState) => state.monsters.monsters;
-
-export const selectSelectedMonster = (state: RootState) =>
-  state.monsters.selectedMonster;
